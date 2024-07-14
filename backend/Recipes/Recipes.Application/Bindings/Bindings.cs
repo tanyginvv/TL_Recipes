bb2c01950558ccabe01764ac66b36c41df699d24
tanyginvv <tanyginvv@mail.ru>
2024-07-14 21:43:47 +0300

    Вынес зависимости репозиториев в Bindings

--- backend/Recipes/Recipes.Application/Bindings/Bindings.cs
+++ backend/Recipes/Recipes.Application/Bindings/Bindings.cs
@@ -3,6 +3,11 @@ using Recipes.Application.Tags;
 using Recipes.Application.Steps;
 using Recipes.Application.Recipes;
 using Recipes.Application.Ingredients;
+using Recipes.Infrastructure.Repositories;
+using Recipes.Infrastructure.Entities.Tags;
+using Recipes.Infrastructure.Context;
+using Recipes.Infrastructure.Entities.Recipes;
+using Recipes.Infrastructure.Entities.Steps;
 
 namespace Recipes.Application
 {
@@ -10,6 +15,12 @@ namespace Recipes.Application
     {
         public static IServiceCollection AddBindings( this IServiceCollection services )
         {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IRecipeRepository, RecipeRepository>();
+            services.AddScoped<IIngredientRepository, IngredientRepository>();
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<IStepRepository, StepRepository>();
+
             services.AddRecipesBindings();
             services.AddTagsBindings();
             services.AddStepsBindings();
--- backend/Recipes/Recipes/Program.cs
+++ backend/Recipes/Recipes/Program.cs
@@ -1,22 +1,11 @@
 using Recipes.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Recipes.Application;
-using Application;
-using Recipes.Infrastructure.Entities.Recipes;
-using Recipes.Infrastructure.Entities.Steps;
-using Recipes.Infrastructure.Entities.Tags;
-using Recipes.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder( args );
 
 builder.Services.AddDbContext<RecipesDbContext>( options =>
     options.UseSqlServer( builder.Configuration.GetConnectionString( "Recipes" ) ) );
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
-builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
-builder.Services.AddScoped<ITagRepository, TagRepository>();
-builder.Services.AddScoped<IStepRepository, StepRepository>();
 
 // Регистрация других зависимостей
 builder.Services.AddControllers();

8cd7fd778a08a9daa7af4a7ab675df91a795d1df
tanyginvv <tanyginvv@mail.ru>
2024-08-06 23:22:25 +0300

    правка конфигов

--- backend/Recipes/Recipes.WebApi/Program.cs
+++ backend/Recipes/Recipes.WebApi/Program.cs
@@ -1,12 +1,12 @@
 using Recipes.Application;
 using Recipes.Infrastructure;
 
-var builder = WebApplication.CreateBuilder( args );
+WebApplicationBuilder builder = WebApplication.CreateBuilder( args );
 
-IConfiguration configuration = new ConfigurationBuilder()
-              .AddJsonFile( "appsettings.json" )
-              .AddJsonFile( $"appsettings.{builder.Environment.EnvironmentName}.json" )
-              .Build();
+builder.Configuration
+    .AddJsonFile( "appsettings.json" )
+    .AddJsonFile( $"appsettings.{Environment.GetEnvironmentVariable( "JSON_CONFIG_NAME" )}.json" )
+    .Build();
 
 builder.Services.AddApplicationBindings();
 builder.Services.AddInfrastructureBindings( builder.Configuration );
